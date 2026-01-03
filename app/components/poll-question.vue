fffffb0e942dbb4239acb01fc148ed17294b2f84
Vladimir Ivakhno <wvovaw0@gmail.com>
2026-01-03 22:44:13 +0500

    change question

--- app/components/poll-question.vue
+++ app/components/poll-question.vue
@@ -1,12 +1,12 @@
 <template>
-  <h1>React is a framework or library?</h1>
+    <h1>Is React a framework or a library?</h1>
 </template>
 
 <style scoped>
 h1 {
-  font-size: var(--font-size-2xl);
-  margin-bottom: var(--space-lg);
-  text-align: center;
-  color: var(--color-text);
+    font-size: var(--font-size-2xl);
+    margin-bottom: var(--space-lg);
+    text-align: center;
+    color: var(--color-text);
 }
 </style>
