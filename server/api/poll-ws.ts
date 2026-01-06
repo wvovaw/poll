4e25197de620afeb0ffe1554fb9f9bdf6fc80e65
Vladimir Ivakhno <wvovaw0@gmail.com>
2026-01-06 14:21:45 +0500

    add ws logs for debug

--- server/api/poll-ws.ts
+++ server/api/poll-ws.ts
@@ -1,16 +1,22 @@
 import { wsManager } from '../utils/websocket-manager'
 
 export default defineWebSocketHandler({
+  upgrade(req) {
+    console.log('Upgrade request accepted', req.headers)
+  },
   async open(peer) {
+    console.log('Open websocket', peer.id)
     wsManager.addPeer(peer)
     await wsManager.sendCurrentPoll(peer)
   },
 
   async message(peer, message) {
+    console.log('Websocket message', peer.id)
     await wsManager.handleMessage(peer, message.text())
   },
 
   close(peer) {
+    console.log('Close websocket', peer.id)
     wsManager.removePeer(peer)
   },
 
